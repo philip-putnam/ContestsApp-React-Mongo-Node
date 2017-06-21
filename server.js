d64628a7459f6391ed8e35530190b65f75ad3bf1
Philip Putnam <putnamp989@gmail.com>
2017-06-21 15:45:21 -0700

    add API router

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import config from './config';
+import apiRouter from './api';
 
 import express from 'express';
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hello express');
-});
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
   console.info('Express listening on port ', config.port)
