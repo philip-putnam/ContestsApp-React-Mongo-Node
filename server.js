866a2404ac35c0f1cf832d25f6d8eeba351e3b23
Philip Putnam <putnamp989@gmail.com>
2017-06-22 13:17:57 -0700

    npm i -S install node-sass-middleware, configure server for scss

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import config from './config';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 import express from 'express';
 const server = express();
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
 
 server.set('view engine', 'pug');
 
@@ -14,5 +21,5 @@ server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
-  console.info('Express listening on port ', config.port)
+  console.info('Express listening on port ', config.port);
 });
--- src/components/App.js
+++ src/components/App.js
@@ -14,9 +14,7 @@ export default class App extends Component {
           <Header message={this.state.pageHeader} />
         </div>
         <div>
-          {this.props.contests.map(contest =>
-            <ContestPreview {...contest} />
-          )}
+          
         </div>
       </div>
     );
