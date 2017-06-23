d258565078a1e06642c8e56d1d1496f06dea2c12
Philip Putnam <putnamp989@gmail.com>
2017-06-23 13:47:58 -0700

    add pushState to App.js for simple routing prelim

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
 
+const pushState = (obj, url) =>
+  window.history.pushState(obj, '', url);
+
 class App extends Component {
   state = {
     pageHeader: 'Test Header',
@@ -10,13 +13,21 @@ class App extends Component {
   componentDidMount() {
 
   }
+  fetchContest = (contestId) => {
+    pushState(
+      { currentContestId: contestId },
+      `/contest/${contestId}`
+    );
+  };
 
   render() {
     return (
       <div>
         <div className='app'>
           <Header message={this.state.pageHeader} />
-          <ContestList contests={this.state.contests} />
+          <ContestList
+            onContestClick={this.fetchContest}
+            contests={this.state.contests} />
         </div>
       </div>
     );
