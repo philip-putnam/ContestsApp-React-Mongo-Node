e5d286ef626f8936c82de4c14b88e328fa90b62a
Philip Putnam <putnamp989@gmail.com>
2017-06-23 14:58:01 -0700

    add content at routed links

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
+import Contest from './Contest';
 
 const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
@@ -10,25 +11,39 @@ class App extends Component {
     pageHeader: 'Test Header',
     contests: this.props.initialContests
   };
+
   componentDidMount() {
 
   }
+
   fetchContest = (contestId) => {
     pushState(
       { currentContestId: contestId },
       `/contest/${contestId}`
     );
+
+    this.setState({
+      pageHeader: this.state.contests[contestId].contestName,
+      currentContestId: contestId
+    });
   };
 
+  currentContent() {
+    if (this.state.currentContestId) {
+      return <Contest {...this.state.contests[this.state.currentContestId]} />;
+    }
+
+    return <ContestList
+      onContestClick={this.fetchContest}
+      contests={this.state.contests} />;
+  }
 
   render() {
     return (
       <div>
         <div className='app'>
           <Header message={this.state.pageHeader} />
-          <ContestList
-            onContestClick={this.fetchContest}
-            contests={this.state.contests} />
+          {this.currentContent()}
         </div>
       </div>
     );
